dc520f671602724ad9619d9f2d0fa29d7d22e31a
Iskandar Setiadi <iskandar.setiadi@hde.co.jp>
2016-09-07 12:42:51 +0900

    Another try to fix build script

--- build.sh
+++ build.sh
@@ -8,10 +8,6 @@ remote=$(git config remote.origin.url)
 # make a directory
 mkdir gh-pages-branch
 
-# we can do something here, e.g., generate required HTML file
-# TODO: Update this line
-python generator.py
-
 cd gh-pages-branch
 
 # Setup a new repo
@@ -29,6 +25,16 @@ then
 else
     git checkout --orphan gh-pages
 fi
+
+# Go back to previous directory
+cd ..
+
+# we can do something here, e.g., generate required HTML file
+# TODO: Update this line
+python generator.py
+
+# Go to gh-pages-branch directory again
+cd gh-pages-branch
 
 # stage any changes and new files
 git add -A
